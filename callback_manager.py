19996f0ad44bc2d9982ba6888821f41370a6caa4
Pam <pamhome21@gmail.com>
2025-02-08 03:09:52 +0500

    Add support for SamplerCustomAdvanced #7
    Add time inputs to DiffusionCG

--- callback_manager.py
+++ callback_manager.py
@@ -1,7 +1,6 @@
 from typing import Callable
 import torch
-import comfy.latent_formats
-import comfy.sample
+from comfy.samplers import KSAMPLER
 
 
 class CallbackManager:
@@ -9,21 +8,29 @@ class CallbackManager:
         self.callbacks: dict[str, tuple[Callable[[tuple], Callable], int]] = {}
 
     def hijack_samplers(self):
-        if not hasattr(comfy.sample, "sample_original"):
-            comfy.sample.sample_original = comfy.sample.sample
-            comfy.sample.sample = self.sample_wrapper(comfy.sample.sample_original)
-        if not hasattr(comfy.sample, "sample_custom_original"):
-            comfy.sample.sample_custom_original = comfy.sample.sample_custom
-            comfy.sample.sample_custom = self.sample_wrapper(comfy.sample.sample_custom_original)
+        sample_original = "sample_original"
+        if not hasattr(KSAMPLER, sample_original):
+            setattr(KSAMPLER, sample_original, KSAMPLER.sample)
+            KSAMPLER.sample = self.sample_wrapper(getattr(KSAMPLER, sample_original))
 
     def register_callback(self, params_name: str, callback_func: Callable[[tuple], Callable], priority: int):
         self.callbacks[params_name] = callback_func, priority
 
-    def sample_wrapper(self, original_sample: Callable):
-        def sample(*args, **kwargs):
-            model = args[0]
+    def sample_wrapper(_self, original_sample: Callable):
+        def sample(
+            self: KSAMPLER,
+            model_wrap,
+            sigmas,
+            extra_args,
+            callback,
+            noise,
+            latent_image=None,
+            denoise_mask=None,
+            disable_pbar=False,
+        ):
+            model = model_wrap
 
-            original_cb = kwargs["callback"]
+            original_cb = callback
             original_cb_priority = 1000
 
             callbacks = []
@@ -32,7 +39,7 @@ class CallbackManager:
                 if cb is not None:
                     callbacks.append((priority, cb))
 
-            for params_name, (cb_wrapper, priority) in self.callbacks.items():
+            for params_name, (cb_wrapper, priority) in _self.callbacks.items():
                 params = model.model_options.get(params_name, None)
                 if params:
                     cb = cb_wrapper(params)
@@ -41,11 +48,20 @@ class CallbackManager:
 
             callbacks.sort()
 
-            def callback(step: int, x0: torch.Tensor, x: torch.Tensor, total_steps: int):
+            def callback_new(step: int, x0: torch.Tensor, x: torch.Tensor, total_steps: int):
                 for _, cb in callbacks:
                     cb(step, x0, x, total_steps)
 
-            kwargs["callback"] = callback
-            return original_sample(*args, **kwargs)
+            return original_sample(
+                self,
+                model_wrap,
+                sigmas,
+                extra_args,
+                callback_new,
+                noise,
+                latent_image,
+                denoise_mask,
+                disable_pbar,
+            )
 
         return sample
--- cg_nodes.py
+++ cg_nodes.py
@@ -11,8 +11,11 @@ class DiffusionCG:
         return {
             "required": {
                 "model": ("MODEL",),
+                "normalize_start_time": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "normalize_end_time": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
                 "recenter_strength": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.05}),
-                "normalize": ("BOOLEAN", {"default": True}),
+                "recenter_start_time": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "recenter_end_time": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
             }
         }
 
@@ -25,16 +28,22 @@ class DiffusionCG:
     def hook(
         self,
         model: ModelPatcher,
+        normalize_start_time: float,
+        normalize_end_time: float,
         recenter_strength: float,
-        normalize: bool,
+        recenter_start_time: float,
+        recenter_end_time: float,
     ):
         m = model.clone()
         latent_format = type(model.model.latent_format)
 
         m.model_options[self.PARAMS_NAME] = (
             latent_format,
+            normalize_start_time,
+            normalize_end_time,
             recenter_strength,
-            normalize,
+            recenter_start_time,
+            recenter_end_time,
         )
 
         return (m,)
@@ -44,12 +53,16 @@ class DiffusionCG:
         def _callback(step: int, x0: Tensor, x: Tensor, total_steps: int):
             (
                 latent_format,
+                normalize_start_time,
+                normalize_end_time,
                 recenter_strength,
-                normalize,
+                recenter_start_time,
+                recenter_end_time,
             ) = params
 
             scale_factor = comfy.latent_formats.SD15().scale_factor  # XL factor is causing noise
             dynamic_range = (1.0 / scale_factor) / 2.0
+            current_time = step / (total_steps - 1)
 
             latent = x0
             target = latent.detach().clone()
@@ -58,10 +71,10 @@ class DiffusionCG:
 
             for b in range(bs):
                 for c in range(cs):
-                    if recenter_strength > 0:
+                    if recenter_strength > 0 and recenter_start_time <= current_time <= recenter_end_time:
                         latent[b][c] += -target[b][c].mean() * recenter_strength
 
-                    if normalize:
+                    if normalize_start_time <= current_time <= normalize_end_time:
                         latent[b][c] = normalize_tensor(latent[b][c], dynamic_range)
 
         return _callback
